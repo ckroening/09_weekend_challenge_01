cb0d823e4fac9cce790679b5a62049cf885e39d1
cdaisyk <charlottekroening@gmail.com>
2015-11-22 15:53:18 -0600

    added and got tests working for getInputs & addEmployee functions

--- src/script.js
+++ src/script.js
@@ -1,17 +1,28 @@
-$(document).ready(function() {
-	$('button').on('click', function() {
-		getInputs();
-	});
-})
 
 var getInputs = function() {
-	return {
-		firstName: $('#firstName').val(),
-		lastName: $('#lastName').val(),
-		employeeNum: $('#employeeNum').val(),
-		title: $('#title').val(),
-		lastReviewScore: $('#lastReviewScore').val(),
-		salary: $('#salary').val()
-	};
+  return {
+    firstName: $('#firstName').val(),
+    lastName: $('#lastName').val(),
+    employeeNum: $('#employeeNum').val(),
+    title: $('#title').val(),
+    lastReviewScore: $('#lastReviewScore').val(),
+    salary: $('#salary').val()
+  };
 };
 
+var addEmployee = function(employee) {
+  var newRow = $('<tr></tr>');
+  newRow.append($('<td></td>').text(employee.firstName));
+  newRow.append($('<td></td>').text(employee.lastName));
+  newRow.append($('<td></td>').text(employee.employeeNum));
+  newRow.append($('<td></td>').text(employee.title));
+  newRow.append($('<td></td>').text(employee.lastReviewScore));
+  newRow.append($('<td></td>').text(employee.salary));
+  $('#employees').append(newRow);
+}
+
+$(document).ready(function() {
+  $('button').on('click', function() {
+    addEmployee(getInputs());
+  });
+})
--- test/script.spec.js
+++ test/script.spec.js
@@ -2,24 +2,92 @@ fixture.setBase('src');
 
 // Load index.html into DOM before each test runs.
 QUnit.module('DOM manipulation', {
-	beforeEach: function() {
-		var mainHtml = fixture.load('index.html');
-		var container = $('<div>')
-			.attr('id', 'fixture')
-			.html(mainHtml[0].innerHTML);
-		$(document.body).append(container);
-	},
-	afterEach: function() {
-		$('#fixture').remove();
-	}
+  beforeEach: function() {
+    var mainHtml = fixture.load('index.html');
+    var container = $('<div>')
+      .attr('id', 'fixture')
+      .html(mainHtml[0].innerHTML);
+    $(document.body).append(container);
+  },
+  afterEach: function() {
+    $('#fixture').remove();
+  }
 });
 
 QUnit.test('getInputs', function(assert) {
-    // Setup the various states of the code you want to test and assert conditions.
-    assert.equal(1, 1, '1 === 1');
-    assert.ok(true, 'true is truthy');
-    assert.ok(1, '1 is also truthy');
-    assert.ok([], 'so is an empty array or object');
-    $('#firstName').val('foo');
-    console.log($('#firstName').val());
+  $('#firstName').val('Ralph');
+  $('#lastName').val('Log');
+  $('#employeeNum').val('5');
+  $('#title').val('go');
+  $('#lastReviewScore').val('2');
+  $('#salary').val('3000');
+  var expected = {
+    firstName: 'Ralph',
+    lastName: 'Log',
+    employeeNum: '5',
+    title: 'go',
+    lastReviewScore: '2',
+    salary: '3000'
+  };
+  var actual = getInputs();
+  assert.deepEqual(expected, actual);
 });
+
+QUnit.test('addEmployee', function(assert) {
+  var rows = $('#employees').children().children();
+  assert.equal(rows.length, 1);
+
+  var employee = {
+    firstName: 'Ralph',
+    lastName: 'Log',
+    employeeNum: '5',
+    title: 'go',
+    lastReviewScore: '2',
+    salary: '3000'
+  }
+  addEmployee(employee);
+  var rows = $('#employees').children().children();
+  assert.equal(rows.length, 2);
+  var columns = rows.first().next().children();
+  var column = columns.first();
+
+  assert.equal(column.text(), 'Ralph');
+  column = column.next();
+  assert.equal(column.text(), 'Log');
+  column = column.next();
+  assert.equal(column.text(), '5');
+  column = column.next();
+  assert.equal(column.text(), 'go');
+  column = column.next();
+  assert.equal(column.text(), '2');
+  column = column.next();
+  assert.equal(column.text(), '3000');
+
+  var employee2 = {
+    firstName: 'George',
+    lastName: 'New',
+    employeeNum: '4',
+    title: 'stop',
+    lastReviewScore: '1',
+    salary: '2000'
+  }
+  addEmployee(employee2);
+  var rows = $('#employees').children().children();
+  assert.equal(rows.length, 3);
+  var columns = rows.first().next().next().children();
+  var column = columns.first();
+
+  assert.equal(column.text(), 'George');
+  column = column.next();
+  assert.equal(column.text(), 'New');
+  column = column.next();
+  assert.equal(column.text(), '4');
+  column = column.next();
+  assert.equal(column.text(), 'stop');
+  column = column.next();
+  assert.equal(column.text(), '1');
+  column = column.next();
+  assert.equal(column.text(), '2000');
+
+})
+
